f0a9aa1d04bf33fb17ae0a58a6f8af5a99cebf09
Fritz Lin <uxfritz@163.com>
2016-10-09 12:12:56 +0800

    0.0.3 - pass npm_config_argv for target build, such as nwjs https://github.com/fritx/unix-sqlcipher/commit/f55b35721c3576c271ef344ca67b43a7a6aee259 https://github.com/mapbox/node-sqlite3/issues/417 https://github.com/mapbox/node-sqlite3/issues/497

--- postinstall.js
+++ postinstall.js
@@ -1,6 +1,22 @@
 'use strict'
 require('shelljs/global')
+var isArray = require('util').isArray
+
+var args
+try {
+  args = JSON.parse(process.env.npm_config_argv).original
+} finally {
+  if (!isArray(args)) {
+    args = []
+  }
+}
+var targetArgs = args.filter(function (arg) {
+  return /^--(runtime|target)/.test(arg)
+})
+var targetStr = targetArgs.reduce(function (m, arg) {
+  return m + ' ' + arg
+}, '')
 
 // windows
 // todo: pass build args
-exec('npm install sqlite3 --build-from-source')
+exec('npm install sqlite3 --build-from-source' + targetStr)
